aef0cfc9cf1b25be019fc05d86733ded4146abd4
Naresh Singh <nareshsingh0170@gmail.com>
2024-05-27 23:08:15 +0530

    Home Page

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+
+function App() {
+  return (
+    <div className="flex min-h-screen w-screen flex-col bg-richblack-900 font-poppins">
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
