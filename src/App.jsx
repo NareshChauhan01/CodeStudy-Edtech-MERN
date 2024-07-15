0dbd31541176cb469200430e3218e43f6bd2d720
Naresh Singh <nareshsingh0170@gmail.com>
2024-07-15 11:50:02 +0530

    Signup,Login and VerifyEmail Page Complete

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,15 @@ import "./App.css"
 
 // React Router
 import { Routes, Route } from 'react-router-dom'
+
 // Components
 import Navbar from './components/Common/Navbar'
 // Pages
 import Home from './pages/Home'
+import OpenRoute from './components/core/Auth/OpenRoute'
+import Signup from './pages/Signup'
+import Login from './pages/Login'
+import VerifyEmail from './pages/VerifyEmail'
 
 function App() {
   return (
@@ -15,6 +20,33 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+
+        <Route
+          path='signup'
+          element={
+            <OpenRoute>
+              <Signup />
+            </OpenRoute>
+          }
+        />
+
+        <Route
+          path='Login'
+          element={
+            <OpenRoute>
+              <Login />
+            </OpenRoute>
+          }
+        />
+
+        <Route
+          path='verify-email'
+          element={
+            <OpenRoute>
+              <VerifyEmail />
+            </OpenRoute>
+          }
+        />
 
       </Routes>
     </div>
