3de2dec81dd406425acef19ae42f695f8beccaad
Naresh Chauhan <nareshsingh0170@gmail.com>
2026-08-12 23:59:48 +0530

    Utils

---
+++ src/utils/constants.js
@@ -0,0 +1,10 @@
+export const COURSE_STATUS = {
+  DRAFT: "Draft",
+  PUBLISHED: "Published"
+}
+
+export const ACCOUNT_TYPE = {
+  STUDENT: "Student",
+  INSTRUCTOR: "Instructor",
+  ADMIN: "Admin",
+}
---
+++ src/utils/dateFormatter.js
@@ -0,0 +1,7 @@
+export const formatedDate = (date) => {
+  return new Date(date).toLocaleDateString("en-US", {
+    month: "long",
+    day: "numeric",
+    year: "numeric"
+  })
+}
