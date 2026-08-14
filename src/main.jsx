b9fb3529f93b181d35a9a5edf780673453d63a6f
Naresh Chauhan <nareshsingh0170@gmail.com>
2026-08-14 22:15:00 +0530

    Toaster add

--- src/main.jsx
+++ src/main.jsx
@@ -13,6 +13,7 @@ createRoot(document.getElementById('root')).render(
     <Provider store={store} >
       <BrowserRouter>
         <App />
+        <Toaster position="top-right" />
       </BrowserRouter>
     </Provider>
 
